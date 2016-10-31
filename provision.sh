a6e0d42339c86965f679ab6f56f8da477724a8de
m8pple <dt10@imperial.ac.uk>
2016-10-31 17:48:07 +0000

    Note about Debian vs Ubuntu.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#NOTE: This is for Debian Jessie, and has not been propertly
+# tested in Ubuntu (and appears not to work)...
+
 sudo sed -i -e "s/contrib$/contrib non-free/g" /etc/apt/sources.list
+sudo sed -i -e "s/main$/main non-free/g" /etc/apt/sources.list
 
 sudo apt-get -y update
 
